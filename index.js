b9ced15db9efdea83d800344e0d4008ff5fef6b6
SleepKnight8 <jehadcoder@gmail.com>
2026-06-27 16:02:35 +0300

    Implement game attempts limit and win condition
    Added tries variable to limit attempts and modified check function to handle game over and winning conditions.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let main_input_pos = {};
 let true_pos = 0;
 let false_pos = 0;
 let false_in = 0;
+let tries = 8;
 
 function random_num_gen(){
     for(i = 0; i < 4; i++){
@@ -17,7 +18,6 @@ function random_num_gen(){
     console.log(solve_pos);
 }
 
-random_num_gen();
 
 function main_input_pos_func() {
         main_input = document.getElementById("main_input").value;
@@ -32,22 +32,29 @@ function main_input_pos_func() {
 
 function check(){
     main_input_pos_func();
-    for (i = 0; i < 4; i++){
-        if (main_input_pos[i] === solve_pos[i]){
-            true_pos += 1;
-        }
-        else if (random_num.includes(main_input_pos[i])){
-            false_pos += 1;
-        }
-        else {
-            false_in += 1;
-        }
+    tries -= 1
+    if (tries < 1){
+        alert("Game over")
+        restart();
+         return;
     }
-    document.getElementById("message").innerHTML = `Correct Pos: ${true_pos}, Wrong Pos: ${false_pos}, Wrong Num: ${false_in}`;
-    document.getElementById("display").innerHTML = `${main_input[0]} ${main_input[1]} ${main_input[2]} ${main_input[3]}`;
-    erase_game_status();
-
-
+    else if (main_input === random_num){
+        alert("You won!");
+        restart();
+         return;
+    }
+        for (i = 0; i < 4; i++) {
+            if (main_input_pos[i] === solve_pos[i]) {
+                true_pos += 1;
+            } else if (random_num.includes(main_input_pos[i])) {
+                false_pos += 1;
+            } else {
+                false_in += 1;
+            }
+        }
+        document.getElementById("message").innerHTML = `Correct Pos: ${true_pos}, Wrong Pos: ${false_pos}, Wrong Num: ${false_in} tries left: ${tries}`;
+        document.getElementById("display").innerHTML = `${main_input[0]} ${main_input[1]} ${main_input[2]} ${main_input[3]}`;
+        erase_game_status();
 }
 
 function erase_game_status(){
@@ -55,3 +62,19 @@ function erase_game_status(){
     false_pos = 0;
     false_in = 0;
 }
+
+
+function restart(){
+    random_num = "";
+    solve_pos = {};
+    main_input_pos = {};
+    true_pos = 0;
+    false_pos = 0;
+    false_in = 0;
+    tries = 8;
+    random_num_gen();
+    document.getElementById("message").innerHTML = `Deduction is the key`;
+    document.getElementById("display").innerHTML = `_ _ _ _`;
+}
+
+random_num_gen();
