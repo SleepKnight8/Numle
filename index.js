326f8da0b89e7a25e82025787718a7fa62c004bb
SleepKnight8 <jehadcoder@gmail.com>
2026-06-27 23:00:05 +0300

    Enhance input validation for main input
    Added input validation for unique digits and length in main_input_pos_func.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let true_pos = 0;
 let false_pos = 0;
 let false_in = 0;
 let tries = 8;
+let in_check_stat = true;
 
 function random_num_gen(){
       while (random_num.length < 4) {
@@ -31,30 +32,49 @@ function random_num_gen(){
 
 
 function main_input_pos_func() {
-        main_input = document.getElementById("main_input").value;
-        for(i = 0; i < 4; i++){
-            let digit2 = main_input[i]
-            main_input_pos[i] = digit2;
-        }
-        //console.log(main_input);
-        //console.log(main_input_pos);
+    main_input = document.getElementById("main_input").value;
+    let main_input_check = new Set();
+    for (let x = 0; x < 4; x++) {
+        let check_dig = main_input[x]
+        main_input_check.add(check_dig);
+    }
+    if (main_input_check.size < 4 && main_input.length === 4) {
+        alert("Can't use the same number more than once");
+        in_check_stat = false;
+        return;
+    }
+    else if (main_input.length !== 4){
+        alert("The code must contain 4 digits");
+        in_check_stat = false;
+        return;
+    }
+    else {
+        in_check_stat = true;
+        for(let i = 0; i < 4; i++){
+        let digit2 = main_input[i]
+        main_input_pos[i] = digit2;
+    }
+    }
+
+    //console.log(main_input);
+    //console.log(main_input_pos);
 }
 
 
 function check(){
     main_input_pos_func();
-    tries -= 1
-    if (tries < 1){
-        alert("Game over")
-        restart();
-         return;
-    }
-    else if (main_input === random_num){
-        alert("You won!");
-        restart();
-         return;
-    }
-        for (i = 0; i < 4; i++) {
+    if (in_check_stat) {
+        tries -= 1
+        if (tries < 1) {
+            alert("Game over")
+            restart();
+            return;
+        } else if (main_input === random_num) {
+            alert("You won!");
+            restart();
+            return;
+        }
+        for (let i = 0; i < 4; i++) {
             if (main_input_pos[i] === solve_pos[i]) {
                 true_pos += 1;
             } else if (random_num.includes(main_input_pos[i])) {
@@ -66,6 +86,7 @@ function check(){
         document.getElementById("message").innerHTML = `Correct Pos: ${true_pos}, Wrong Pos: ${false_pos}, Wrong Num: ${false_in} tries left: ${tries}`;
         document.getElementById("display").innerHTML = `${main_input[0]} ${main_input[1]} ${main_input[2]} ${main_input[3]}`;
         erase_game_status();
+    }
 }
 
 function erase_game_status(){
@@ -83,9 +104,11 @@ function restart(){
     false_pos = 0;
     false_in = 0;
     tries = 8;
+    in_check_stat = true;
     random_num_gen();
     document.getElementById("message").innerHTML = `Deduction is the key`;
     document.getElementById("display").innerHTML = `_ _ _ _`;
+
 }
 
 random_num_gen();
